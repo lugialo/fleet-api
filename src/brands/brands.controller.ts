71bad29d89a1065f3576dc15cdc4dcfa8bb3e9d2
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2026-06-05 09:35:22 -0300

    feat: BrandController

--- src/brands/brands.controller.ts
+++ src/brands/brands.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import * as currentUserDecorator from '../auth/decorators/current-user.decorator';
+import { BrandsService } from './brands.service';
+import { CreateBrandDto } from './dto/create-brand.dto';
+import { UpdateBrandDto } from './dto/update-brand.dto';
 
 @Controller('brands')
-export class BrandsController {}
+export class BrandsController {
+  constructor(private readonly brandsService: BrandsService) {}
+
+  @Post()
+  create(
+    @Body() dto: CreateBrandDto,
+    @currentUserDecorator.CurrentUser()
+    user: currentUserDecorator.AuthenticatedUser,
+  ) {
+    return this.brandsService.create(dto, user.id);
+  }
+
+  @Get()
+  findAll() {
+    return this.brandsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.brandsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: UpdateBrandDto) {
+    return this.brandsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.brandsService.remove(id);
+  }
+}
--- src/brands/brands.module.ts
+++ src/brands/brands.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 import { BrandsController } from './brands.controller';
 import { BrandsService } from './brands.service';
+import { Brand } from './entities/brand.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Brand, User])],
   controllers: [BrandsController],
-  providers: [BrandsService]
+  providers: [BrandsService],
+  exports: [BrandsService],
 })
 export class BrandsModule {}
