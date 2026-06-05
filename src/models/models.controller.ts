40e649f1db3c47b4ac5cee94e5b4dd509aeb5b7f
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2026-06-05 10:50:26 -0300

    feat: ModelsController

--- src/models/models.controller.ts
+++ src/models/models.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import * as currentUserDecorator from '../auth/decorators/current-user.decorator';
+import { CreateModelDto } from './dto/create-model.dto';
+import { UpdateModelDto } from './dto/update-model.dto';
+import { ModelsService } from './models.service';
 
 @Controller('models')
-export class ModelsController {}
+export class ModelsController {
+  constructor(private readonly modelsService: ModelsService) {}
+
+  @Post()
+  create(
+    @Body() dto: CreateModelDto,
+    @currentUserDecorator.CurrentUser()
+    user: currentUserDecorator.AuthenticatedUser,
+  ) {
+    return this.modelsService.create(dto, user.id);
+  }
+
+  @Get()
+  findAll() {
+    return this.modelsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.modelsService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseUUIDPipe) id: string, @Body() dto: UpdateModelDto) {
+    return this.modelsService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.modelsService.remove(id);
+  }
+}
--- src/models/models.module.ts
+++ src/models/models.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ModelsService } from './models.service';
 import { ModelsController } from './models.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Model } from './entities/model.entity';
+import { Brand } from 'src/brands/entities/brand.entity';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Model, Brand, User])],
   providers: [ModelsService],
-  controllers: [ModelsController]
+  controllers: [ModelsController],
+  exports: [ModelsService],
 })
 export class ModelsModule {}
