d5e3a0d7f0b043e5f0727f45ac58337b7ebe9988
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2026-06-05 11:30:18 -0300

    feat: VehiclesController

--- src/vehicles/vehicles.controller.ts
+++ src/vehicles/vehicles.controller.ts
@@ -1,4 +1,51 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import * as currentUserDecorator from '../auth/decorators/current-user.decorator';
+import { CreateVehicleDto } from './dto/create-vehicle.dto';
+import { UpdateVehicleDto } from './dto/update-vehicle.dto';
+import { VehiclesService } from './vehicles.service';
 
 @Controller('vehicles')
-export class VehiclesController {}
+export class VehiclesController {
+  constructor(private readonly vehiclesService: VehiclesService) {}
+
+  @Post()
+  create(
+    @Body() dto: CreateVehicleDto,
+    @currentUserDecorator.CurrentUser()
+    user: currentUserDecorator.AuthenticatedUser,
+  ) {
+    return this.vehiclesService.create(dto, user.id);
+  }
+
+  @Get()
+  findAll() {
+    return this.vehiclesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.vehiclesService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateVehicleDto,
+  ) {
+    return this.vehiclesService.update(id, dto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseUUIDPipe) id: string) {
+    return this.vehiclesService.remove(id);
+  }
+}
--- src/vehicles/vehicles.module.ts
+++ src/vehicles/vehicles.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { VehiclesService } from './vehicles.service';
 import { VehiclesController } from './vehicles.controller';
+import { Model } from 'src/models/entities/model.entity';
+import { User } from 'src/users/entities/user.entity';
+import { Vehicle } from './entities/vehicle.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Vehicle, Model, User])],
   providers: [VehiclesService],
-  controllers: [VehiclesController]
+  controllers: [VehiclesController],
+  exports: [VehiclesService],
 })
 export class VehiclesModule {}
